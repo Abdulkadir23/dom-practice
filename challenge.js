3a14d7e7caaa1e580f72b582183c7c898afd5b49
= <=rietch11@hotmail.co.uk>
2023-10-09 20:20:17 +0100

    multiply

---
+++ challenge.js
@@ -0,0 +1,53 @@
+// for(let i = 0; i <20; i++){
+// console.log("Abdulkadir")
+// }
+
+
+
+// let i = 0;
+// while(i > 20){
+//     console.log("Mohamed");
+//     i++;
+// }
+
+
+// let i = 0;
+// do{
+//     console.log("Mohamed");
+//     i++;
+// }
+// while(i <5);
+
+
+// let today = "tuesday"
+// if (today == "monday") {
+//     console.log("it is coding day")
+// }else if(today =="tuesday"){
+//     console.log("it is my of day")
+// }else{
+//     console.log(" ask me personally which day")
+// }
+
+// switch (today) {
+//     case "monday":
+//     console.log("it is coding day, from switch")
+//     break;
+// case "tuesday":
+//     console.log("it is my off day, from switch")
+//     break
+//     console.log("ask me personally which day, from switch")
+
+// }
+
+function multiply (num1,num2){
+    let result = num1 * num2;
+    return result;
+}
+let sum = multiply(5,6)
+console.log(sum)
+
+function multiply (num1,num2){
+    return num1*num2
+}
+console.log (multiply(7,9))
+
